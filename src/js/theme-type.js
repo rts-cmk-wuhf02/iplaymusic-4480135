2a41723009f2b2059176856b9cd3b17777868462
4480135 <4480135@RTS-365.dk>
2020-01-17 13:02:06 +0100

    Add theme button functionality

--- src/js/theme-type.js
+++ src/js/theme-type.js
@@ -4,8 +4,20 @@ const getCookie = function(name) {
     if (parts.length == 2) return parts.pop().split(";").shift();
 }
 
+const iconContrastDom = document.querySelector("#icon-contrast");
+
 document.addEventListener('DOMContentLoaded', function() {
     if(getCookie("light-theme") == "true") {
         document.body.classList.add("light-theme");
     }
+
+    iconContrastDom.addEventListener('click', function() {
+        if(getCookie("light-theme") != "true") {
+            document.cookie = "light-theme=true";
+            document.body.classList.add("light-theme");
+        } else {
+            document.cookie = "light-theme=false";
+            document.body.classList.remove("light-theme");
+        }
+    });
 });
