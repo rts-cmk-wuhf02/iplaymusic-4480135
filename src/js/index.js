975e7095455bfd6e82019957c35adc4ccc55edc8
4480135 <4480135@RTS-365.dk>
2020-01-25 10:51:55 +0100

    Implement lazyload for featured page

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 let authToken;
 
-let swiper;
+let observer;
 
 // Data
 let dataPlaylists;
@@ -65,20 +65,33 @@ function showData() {
 
         featuredContainerDom.innerHTML = "";
 
+        observer = new IntersectionObserver(function (entries) {
+            entries.forEach(entry => {
+                if(entry.target.getAttribute("data-loaded") !== true && entry.isIntersecting) {
+                    entry.target.setAttribute("data-loaded", "true");
+                    entry.target.querySelector(".featured-item").style = `background-image: url("${entry.target.getAttribute("data-src")}");`;
+                }
+            });
+        }, {
+            root: null, // avoiding 'root' or setting it to 'null' sets it to default value: viewport
+            rootMargin: '0px',
+            threshold: 0
+        });
+
         for(let i = 0; i < albumItems.length; i++) {
             const albumElement = templateFeaturedItemDom.content.cloneNode(true);
-
+            
             albumElement.querySelector(".item-link").href = "/album_details/?id=" + albumItems[i].id;
-            albumElement.querySelector(".featured-item").style = `background-image: url("${albumItems[i].images[0].url}");`;
             albumElement.querySelector(".item-title").textContent = albumItems[i].name;
             albumElement.querySelector(".item-subtitle").textContent = albumItems[i].artists[0].name;
-
-
+            
             featuredContainerDom.appendChild(albumElement);
+            
+
+            const albumDom = featuredContainerDom.children[featuredContainerDom.children.length - 1];
+            albumDom.setAttribute("data-src", albumItems[i].images[0].url);
+
+            observer.observe(albumDom);
         }
     });
-}
-
-function updateData() {
-    
 }
